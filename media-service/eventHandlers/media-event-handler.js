9f291c7accea648de1848e9bffcd71b1cb0ec11c
Getange <emmanuelgetange48@gmail.com>
2025-05-17 00:58:36 +0300

    chore:RabbitMQ-debug(making data durable)

--- media-service/eventHandlers/media-event-handler.js
+++ media-service/eventHandlers/media-event-handler.js
@@ -1,5 +1,23 @@
-// events being handled
+import { Media } from "../models/media";
+import { deleteMediaFromCloudinary } from "../utils/cloudinary";
+import { logger } from "../utils/logger.js"
 
+// events being handled
 export const handlePostDeleted = async (event)=>{
     console.log(event, "eventevent")
+
+     const { postId, mediaIds } = event;
+
+     try {
+        const mediaToDelete = await Media.find({_id: {$in: mediaIds}})
+
+        for( const media of mediaToDelete){
+            await deleteMediaFromCloudinary(media.publicId)
+            await Media.findByIdAndDelete(media._id)
+
+            logger.info(`Deleted media ${media._id} associated with this postId: ${postId}`)
+        }
+     } catch (error) {
+        logger.error(`Error occured while deleting media`, error)
+     }
 }
